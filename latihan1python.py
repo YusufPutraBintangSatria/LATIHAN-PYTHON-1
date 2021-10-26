2780430b1233d2e84a8e198ee8eff3859ac1d529
Yusuf Putra Bintang Satria <92704969+YusufPutraBintangSatria@users.noreply.github.com>
2021-10-25 16:36:21 -1200

    Add files via upload

--- latihan1python.py
+++ latihan1python.py
@@ -1,3 +1,13 @@
-print ("Assalamu'alaikum")
-print ("Let me introduce my self. My name is Yusuf Putra Bintang Satria. But you can call me Yusuf/Bintang/Satria. My NPM is 312110317. I was born in Jakarta and I am 18 years old. I am very glad if you want to invite my house in Jl. Jagasatru Asrama Brimob rt 001/008 Desa Hegarmukti Kec. Cikarang Pusat Kab. Bekasi. So don't forget to call me before with the number 082124262822")
-print ("Thank you")
+pembuka = "Assalamu'alaikum"
+nama_lengkap  = 'Yusuf Putra Bintang Satria'
+nama_panggilan = 'Yusuf or Bintang or Satria'
+npm = '312110317'
+tempat_lahir = 'Jakarta'
+umur = '18'
+alamat = 'Jl.Jagasatru Asrama Brimob, rt 001/008, desa Hegarmukti, Kecamatan Cikarang Pusat, Kabupaten Bekasi'
+telepon = '082124262822'
+penutup = 'Thank you'
+print(pembuka)
+print('Let me introduce my self. my name is ' + nama_lengkap +', but you can call me ' + nama_panggilan+'. My NPM is ' + npm)
+print('I was born in ' + tempat_lahir +' and i am ' + umur + ' years old' +'. I am very glad if you want to invite my house in ' + alamat +". So don't forget to call me before with the number " + telepon)
+print (penutup)
